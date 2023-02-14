dd90c70b7f8a0e6734f78c930d25a1665225649c
vicki962 <ushanshanilka80@gmail.com>
2023-02-14 21:01:51 +0530

    Initial commit

---
+++ src/main/java/com/bumblebee/bumblebeebackend/BumblebeeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bumblebee.bumblebeebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BumblebeeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BumblebeeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bumblebee/bumblebeebackend/BumblebeeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bumblebee.bumblebeebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BumblebeeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
